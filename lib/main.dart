d695c4f9ca75970f792bf0582142da473085cb43
Peilong Li <straydragonl@foxmail.com>
2019-05-02 22:49:18 +0800

    update rudely

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:daily/widget_timer.dart';
 
 void main(List<String> args) => runApp(AppSkeleton());
 
@@ -7,7 +9,7 @@ class AppSkeleton extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: AppHomePage(),
-      theme: ThemeData.dark(),
+      theme: ThemeData.light(),
     );
   }
 }
@@ -18,9 +20,36 @@ class AppHomePage extends StatefulWidget {
 }
 
 class AppHomePageState extends State<AppHomePage> {
+  FlutterLocalNotificationsPlugin notificationsPlugin;
+
+  @override
+  void initState() {
+    super.initState();
+    notificationsPlugin = FlutterLocalNotificationsPlugin();
+    notificationsPlugin.initialize(
+      InitializationSettings(
+        AndroidInitializationSettings("@mipmap/ic_launcher"),
+        IOSInitializationSettings(),
+      ),
+      onSelectNotification: (String payload) async =>
+      await showDialog(
+        context: context,
+        builder: (_) =>
+            AlertDialog(
+              title: Text("balabala"),
+              content: Text("labalaba"),
+            ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("标题"),
+      ),
+      body: CountDownWidget(),
       drawer: Drawer(
         child: Column(
           children: <Widget>[
@@ -29,6 +58,24 @@ class AppHomePageState extends State<AppHomePage> {
               title: Text('title'),
               onTap: () => print("tapped!"),
             ),
+            RaisedButton(
+              onPressed: () async =>
+              await notificationsPlugin.show(
+                0,
+                'balabala,,',
+                'body',
+                NotificationDetails(
+                  AndroidNotificationDetails(
+                    'channel id',
+                    'channel name',
+                    'channel description',
+                  ),
+                  IOSNotificationDetails(),
+                ),
+                payload: 'payload',
+              ),
+              child: Text("Touch me"),
+            )
           ],
         ),
       ),
---
+++ lib/utils.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:meta/meta.dart';
+
+class PluginManager {}
+
+class NotificationsPluginProvider {
+  static NotificationsPluginProvider init({
+    @required String iconNameOfAndroid,
+    @required callback,
+  }) {
+    return _singleton;
+  }
+
+  static final NotificationsPluginProvider _singleton =
+      NotificationsPluginProvider._internal();
+
+  FlutterLocalNotificationsPlugin notificationsPlugin;
+
+  NotificationsPluginProvider._internal() {
+    notificationsPlugin = FlutterLocalNotificationsPlugin();
+    var android = AndroidInitializationSettings("@mipmap/ic_launcher");
+    var ios = IOSInitializationSettings();
+    var initSetting = InitializationSettings(android, ios);
+    notificationsPlugin.initialize(initSetting,
+        onSelectNotification: onSelectNotification);
+  }
+
+  Future onSelectNotification(String payload) async {}
+}
