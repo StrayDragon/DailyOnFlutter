0d258dd5dfa5da9f96f4d932f594eb5fad446f06
straydragon <straydragonl@foxmail.com>
2019-05-06 16:05:06 +0800

    update bloc example test

---
+++ test/study_bloc_pattern_test.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc/bloc.dart';
+
+main() {
+  group('CounterBloc', () {
+    CounterBloc counterBloc;
+    setUp(() => counterBloc = CounterBloc());
+
+    test('初始化状态为0', () {
+      expect(counterBloc.initialState, 0);
+    });
+
+    test('调用单次increment,状态为1', () {
+      final List<int> expected = [0, 1];
+
+      expectLater(
+        counterBloc.state,
+        emitsInOrder(expected),
+      );
+
+      counterBloc.dispatch(CounterEvent.increment);
+    });
+
+    test('调用单次decrement,状态为-1', () {
+      final List<int> expected = [0, -1];
+
+      expectLater(
+        counterBloc.state,
+        emitsInOrder(expected),
+      );
+
+      counterBloc.dispatch(CounterEvent.decrement);
+    });
+  });
+}
+
+enum CounterEvent { increment, decrement }
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  @override
+  int get initialState => 0;
+
+  @override
+  Stream<int> mapEventToState(
+    CounterEvent event,
+  ) async* {
+    switch (event) {
+      case CounterEvent.increment:
+        yield currentState + 1;
+        break;
+      case CounterEvent.decrement:
+        yield currentState - 1;
+        break;
+    }
+  }
+}
