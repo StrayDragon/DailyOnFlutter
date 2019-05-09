65303025cad2f63c241f13f14faccd613e5ede67
Peilong Li <straydragonl@foxmail.com>
2019-05-09 23:21:15 +0800

    fixed the CardsView's index bug basically

--- lib/main.dart
+++ lib/main.dart
@@ -51,12 +51,12 @@ List<String> gImageTitles = [
 
 /// TODO:[ Refactor ] Will extract to no side effects component...
 class HorizontalCards extends StatefulWidget {
-  final List<String> backgroundImages;
+  final List<String> backgroundImageUrls;
   final List<String> title;
 
   HorizontalCards({
     Key key,
-    this.backgroundImages,
+    this.backgroundImageUrls,
     this.title,
   }) : super(key: key);
 
@@ -67,6 +67,8 @@ class HorizontalCards extends StatefulWidget {
 class _HorizontalCardsState extends State<HorizontalCards> {
   PageController _controller;
   double _currentCards = gImages.length - 1.0;
+
+  int _currentCardIndex = 0;
 
   @override
   void initState() {
@@ -84,18 +86,27 @@ class _HorizontalCardsState extends State<HorizontalCards> {
     return Container(
       child: Stack(
         children: <Widget>[
+          CardScrollWidget(_currentCards),
           Positioned.fill(
-            child: PageView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: gImages.length,
-              controller: _controller,
-              reverse: true,
-              itemBuilder: (context, index) {
-                return Container();
+            top: 20.0,
+            left: 20.0,
+            right: 80.0,
+            bottom: 20.0,
+            child: GestureDetector(
+              onTap: () {
+                debugPrint('tapped card index ${_controller.page.toInt()}');
               },
+              child: PageView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: gImages.length,
+                controller: _controller,
+                reverse: true,
+                itemBuilder: (context, index) {
+                  return Container();
+                },
+              ),
             ),
           ),
-          CardScrollWidget(_currentCards),
         ],
       ),
     );
@@ -118,36 +129,36 @@ class CardScrollWidget extends StatelessWidget {
       aspectRatio: gWidgetAspectRatio,
       child: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints boxConstraints) {
-            var width = boxConstraints.maxWidth;
-            var height = boxConstraints.maxHeight;
+        var width = boxConstraints.maxWidth;
+        var height = boxConstraints.maxHeight;
 
-            var safeWidth = width - 2 * padding;
-            var safeHeight = height - 2 * padding;
+        var safeWidth = width - 2 * padding;
+        var safeHeight = height - 2 * padding;
 
-            var heightOfPrimaryCard = safeHeight;
-            var widthOfPrimaryCard = heightOfPrimaryCard * gCardAspectRatio;
+        var heightOfPrimaryCard = safeHeight;
+        var widthOfPrimaryCard = heightOfPrimaryCard * gCardAspectRatio;
 
-            var primaryCardLeft = safeWidth - widthOfPrimaryCard;
-            var horizontalInset = primaryCardLeft / 2;
+        var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+        var horizontalInset = primaryCardLeft / 2;
 
-            List<Widget> cardList = [];
+        List<Widget> cardList = [];
 
-            for (var i = 0; i < gImages.length; i++) {
-              var delta = i - currentPage;
-              bool isOnRight = delta > 0;
+        for (var i = 0; i < gImages.length; i++) {
+          var delta = i - currentPage;
+          bool isOnRight = delta > 0;
 
-              var start = padding +
-                  math.max(
-                      primaryCardLeft -
-                          horizontalInset * -delta * (isOnRight ? 15 : 1),
-                      0.0);
+          var start = padding +
+              math.max(
+                  primaryCardLeft -
+                      horizontalInset * -delta * (isOnRight ? 15 : 1),
+                  0.0);
 
-              cardList.add(makeCardView(delta, start, i));
-            }
-            return Stack(
-              children: cardList,
-            );
-          }),
+          cardList.add(makeCardView(delta, start, i));
+        }
+        return Stack(
+          children: cardList,
+        );
+      }),
     );
   }
 
@@ -183,7 +194,7 @@ class CardScrollWidget extends StatelessWidget {
                   children: <Widget>[
                     Padding(
                       padding:
-                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                          EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
                       child: Text(
                         gImageTitles[i], // FIXME:title
                         style: TextStyle(
