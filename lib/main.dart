59a018c1e8f41f529d420ee086682085227f9618
Peilong Li <straydragonl@foxmail.com>
2019-05-11 13:15:24 +0800

    divide some views and update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,218 +1,64 @@
-import 'dart:math' as math;
-
+import 'package:daily/app/view/components/stacking_cards_view.dart';
+import 'package:daily/testing_my_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  static const String ROUTE_NAME = '/';
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Daily',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(),
+      theme: ThemeData.dark(),
+      initialRoute: TestingWidgetHomePage.ROUTE_NAME,
+      routes: {
+        TestingWidgetHomePage.ROUTE_NAME: (BuildContext context) =>
+            TestingWidgetHomePage(),
+      },
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: HorizontalCards(),
+      appBar: AppBar(
+        title: Text("Daily"),
+        centerTitle: true,
       ),
-    );
-  }
-}
-
-final double gCardAspectRatio = 12.0 / 16.0;
-final double gWidgetAspectRatio = gCardAspectRatio * 1.2;
-
-List<String> gImages = [
-  "assets/image_04.jpg",
-  "assets/image_03.jpg",
-  "assets/image_02.jpg",
-  "assets/image_01.png",
-];
-List<String> gImageTitles = [
-  "挑战 四: PAT甲级训练",
-  "挑战 三: 英语阅读训练",
-  "挑战 二: 英语听力训练",
-  "挑战 一: 高数12题训练",
-];
-
-/// TODO:[ Refactor ] Will extract to no side effects component...
-class HorizontalCards extends StatefulWidget {
-  final List<String> backgroundImageUrls;
-  final List<String> title;
-
-  HorizontalCards({
-    Key key,
-    this.backgroundImageUrls,
-    this.title,
-  }) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => _HorizontalCardsState();
-}
-
-class _HorizontalCardsState extends State<HorizontalCards> {
-  PageController _controller;
-  double _currentCards = gImages.length - 1.0;
-
-  int _currentCardIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = PageController(initialPage: gImages.length - 1);
-    _controller.addListener(() {
-      setState(() {
-        _currentCards = _controller.page;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: <Widget>[
-          CardScrollWidget(_currentCards),
-          Positioned.fill(
-            top: 20.0,
-            left: 20.0,
-            right: 80.0,
-            bottom: 20.0,
-            child: GestureDetector(
-              onTap: () {
-                debugPrint('tapped card index ${_controller.page.toInt()}');
-              },
-              child: PageView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: gImages.length,
-                controller: _controller,
-                reverse: true,
-                itemBuilder: (context, index) {
-                  return Container();
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CardScrollWidget extends StatelessWidget {
-  static final double gCardAspectRatio = 12.0 / 16.0;
-  static final double gWidgetAspectRatio = gCardAspectRatio * 1.2;
-
-  final double currentPage;
-  final double padding = 20.0;
-  final double verticalInset = 20.0;
-
-  CardScrollWidget(this.currentPage);
-
-  @override
-  Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: gWidgetAspectRatio,
-      child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints boxConstraints) {
-        var width = boxConstraints.maxWidth;
-        var height = boxConstraints.maxHeight;
-
-        var safeWidth = width - 2 * padding;
-        var safeHeight = height - 2 * padding;
-
-        var heightOfPrimaryCard = safeHeight;
-        var widthOfPrimaryCard = heightOfPrimaryCard * gCardAspectRatio;
-
-        var primaryCardLeft = safeWidth - widthOfPrimaryCard;
-        var horizontalInset = primaryCardLeft / 2;
-
-        List<Widget> cardList = [];
-
-        for (var i = 0; i < gImages.length; i++) {
-          var delta = i - currentPage;
-          bool isOnRight = delta > 0;
-
-          var start = padding +
-              math.max(
-                  primaryCardLeft -
-                      horizontalInset * -delta * (isOnRight ? 15 : 1),
-                  0.0);
-
-          cardList.add(makeCardView(delta, start, i));
-        }
-        return Stack(
-          children: cardList,
-        );
-      }),
-    );
-  }
-
-  Positioned makeCardView(double delta, double start, int i) {
-    return Positioned.directional(
-      top: padding + verticalInset * math.max(-delta, 0.0),
-      bottom: padding + verticalInset * math.max(-delta, 0.0),
-      start: start,
-      textDirection: TextDirection.rtl,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16.0),
-        child: Container(
-          decoration: BoxDecoration(color: Colors.white, boxShadow: [
-            BoxShadow(
-                color: Colors.black12,
-                offset: Offset(3.0, 6.0),
-                blurRadius: 10.0)
-          ]),
-          child: AspectRatio(
-            aspectRatio: gCardAspectRatio,
-            child: Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                Image.asset(
-                  gImages[i],
-                  fit: BoxFit.cover,
-                ),
-                Flex(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  textDirection: TextDirection.ltr,
-                  direction: Axis.vertical,
-                  children: <Widget>[
-                    Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                      child: Text(
-                        gImageTitles[i], // FIXME:title
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                  ],
-                )
-              ],
+      body: Column(children: <Widget>[
+        SizedBox(
+          height: 30,
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              StackingCardsView().toStringShort() + ":",
+              style: TextStyle(fontSize: 20),
             ),
           ),
         ),
-      ),
+        StackingCardsView(),
+        SizedBox(
+          height: 30,
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              TestingWidgetHomePage().toStringShort() + ":",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+        RaisedButton(
+          child: Text("测试我的Widget"),
+          onPressed: () {
+            Navigator.of(context).pushNamed(TestingWidgetHomePage.ROUTE_NAME);
+          },
+        )
+      ]),
     );
   }
 }
---
+++ lib/testing_my_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class TestingWidgetHomePage extends StatelessWidget {
+  static const String ROUTE_NAME = '/testing-my-widget';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(ROUTE_NAME.substring(1)),
+        centerTitle: true,
+      ),
+      body: PageViewPage(),
+    );
+  }
+}
+
+class PageViewPage extends StatelessWidget {
+  final List<String> _pageName = <String>["第一页", '第二页', '第三页'];
+
+  List<Widget> _makePages() {
+    return List.generate(
+      _pageName.length,
+      (index) => Container(
+            color: Colors.blue[300],
+            alignment: Alignment(0.0, 0.0),
+            child: GestureDetector(
+              onTap: () {
+                debugPrint("按了  ${_pageName[index]}");
+              },
+              child: Text(
+                "${_pageName[index]}",
+                style: TextStyle(
+                  fontSize: 32.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: _makePages(),
+    );
+  }
+}
