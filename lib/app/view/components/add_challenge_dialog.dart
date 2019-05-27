709a46ff28f61b01758800cec80d6bddf5be09b6
Peilong Li <straydragonl@foxmail.com>
2019-05-27 23:32:14 +0800

    modify the appearance for AddChallengeDialog

--- lib/app/view/components/add_challenge_dialog.dart
+++ lib/app/view/components/add_challenge_dialog.dart
@@ -1,91 +1,64 @@
 import 'package:flutter/material.dart';
 
-class AddChallengeDialog extends StatelessWidget {
+class AddChallengeDialog extends StatefulWidget {
   static const String heroTag = "AddChallengeHeroTag";
+
+  @override
+  _AddChallengeDialogState createState() => _AddChallengeDialogState();
+}
+
+class _AddChallengeDialogState extends State<AddChallengeDialog> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: SimpleDialog(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(32.0),
+            )),
         children: <Widget>[
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            // To make the card compact
-            children: <Widget>[
-              Hero(
-                tag: AddChallengeDialog.heroTag,
-                child: CircleAvatar(
-                  backgroundColor: Colors.blue,
-                  radius: 32.0,
-                  child: Text(
-                    "挑战",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w700,
-                    ),
+          Hero(
+            tag: AddChallengeDialog.heroTag,
+            child: CircleAvatar(
+              backgroundColor: Colors.blue,
+              radius: 32.0,
+              child: Text(
+                "挑战",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        debugPrint("OK");
+                      }
+                    },
+                    child: Text('提交'),
                   ),
                 ),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                "ok这是测试啊啊啊啊啊啊啊啊啊",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-              SizedBox(height: 24.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Your Name',
-                ),
-                onSaved: (val) {},
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                ),
-                obscureText: true,
-                validator: (val) {
-                  return val.length < 4 ? "密码长度错误" : null;
-                },
-                onSaved: (val) {},
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Your Name',
-                ),
-                onSaved: (val) {},
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                ),
-                obscureText: true,
-                validator: (val) {
-                  return val.length < 4 ? "密码长度错误" : null;
-                },
-                onSaved: (val) {},
-              ),
-              Align(
-                alignment: Alignment.bottomRight,
-                child: FlatButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (ctx) => SimpleDialog(
-                              children: <Widget>[
-                                Text("可以嵌套对话框(Dialog)"),
-                                Text("!看了源码知道原来是利用Navigator::push实现的"),
-                                Text("233333333333333333"),
-                              ],
-                            ));
-                  },
-                  child: Text("确定"),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
