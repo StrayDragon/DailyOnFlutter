8697454ad1dd6623d0e831a61f42825fe1d3a3a5
straydragon <straydragonl@foxmail.com>
2019-06-02 21:51:11 +0800

    update the dartdoc

--- lib/app/util/json_annotation.dart
+++ lib/app/util/json_annotation.dart
@@ -4,36 +4,36 @@ import 'existing_type.dart';
 
 /// package: `json_annotation`'s simple extension for some special cases
 class JsonKeys {
-  /// Equals to JsonKey(name: '_id')
+  /// Equals to `@JsonKey(name: '_id')`
   static const JsonKey safetyIdName = JsonKey(name: '_id');
 
-  /// Equals to JsonKey(fromJson: BoolUtil.fromJson, toJson: BoolUtil.toJson)
+  /// Equals to `@JsonKey(fromJson: BoolUtil.fromJson, toJson: BoolUtil.toJson)`
   ///
   /// Refer to: [BoolUtil]
   static const JsonKey boolean =
       JsonKey(fromJson: BoolUtil.fromJson, toJson: BoolUtil.toJson);
 
-  /// Equals to JsonKey(fromJson: BoolUtil.fromJson, toJson: BoolUtil.toJson)
+  /// Equals to `@JsonKey(fromJson: BoolUtil.fromJson, toJson: BoolUtil.toJson)`
   ///
-  /// **WARNNING**: Use keyword `bool` maybe contrary to your cognition and code highlight, so just use [JsonKeys.boolean]
+  /// **WARNNING**: Using the keyword `bool` as field name maybe contrary to your cognition and code highlight, so just use [JsonKeys.boolean] instead.
   ///
   /// Refer to: [BoolUtil]
   static const JsonKey bool =
       JsonKey(fromJson: BoolUtil.fromJson, toJson: BoolUtil.toJson);
 
-  /// Equals to JsonKey(fromJson: BoolUtil.fromJson, toJson: BoolUtil.toJson)
+  /// Equals to `@JsonKey(fromJson: BoolUtil.fromJson, toJson: BoolUtil.toJson)`
   ///
   /// Refer to: [BoolUtil]
   static const JsonKey color =
       JsonKey(fromJson: ColorUtil.fromJson, toJson: ColorUtil.toJson);
 
-  /// Equals to JsonKey(fromJson: DateTimeUtil.fromJson, toJson: DateTimeUtil.toJson)
+  /// Equals to `@JsonKey(fromJson: DateTimeUtil.fromJson, toJson: DateTimeUtil.toJson)`
   ///
   /// Refer to: [DateTimeUtil]
   static const JsonKey dateTime =
       JsonKey(fromJson: DateTimeUtil.fromJson, toJson: DateTimeUtil.toJson);
 
-  /// Equals to JsonKey(fromJson: DurationUtil.fromJson, toJson: DurationUtil.toJson)
+  /// Equals to `@JsonKey(fromJson: DurationUtil.fromJson, toJson: DurationUtil.toJson)`
   ///
   /// Refer to: [DurationUtil]
   static const JsonKey duration =
